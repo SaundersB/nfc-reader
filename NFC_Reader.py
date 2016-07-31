390dfc0a1bc30bc52135ce7d4f4930323294add3
SaundersB <speakeasys@gmail.com>
2016-07-31 11:04:11 -0700

    Working on adding writing support

--- NFC_Reader.py
+++ NFC_Reader.py
@@ -6,6 +6,13 @@ from smartcard.CardRequest import CardRequest
 from smartcard.CardConnectionObserver import CardConnectionObserver
 import time
 
+
+COMMAND = [0xFF, 0xCA, 0x00, 0x00, 0x00]
+SELECT = [0x00, 0xA4, 0x04, 0x00, 0x0A, 0xA0, 0x00, 0x00, 0x00, 0x62,
+    0x03, 0x01, 0x0C, 0x06, 0x01]
+COMMAND2 = [0x00, 0x00, 0x00, 0x00]
+
+
 class NFC_Reader():
     def __init__(self, uid = ""):
         self.uid = uid
@@ -42,21 +49,36 @@ class NFC_Reader():
             time.sleep(1)
 
     def read_card_data(self):
+        # Get Response
         try:
-            self.hresult, self.response = SCardTransmit(self.hcard, self.dwActiveProtocol,[0xA0, 0xA4, 0x00, 0x00, 0x02, 0x7F, 0x10])
+            print("Reading card data")
+            self.hresult, self.response = SCardTransmit(self.hcard, self.dwActiveProtocol,[0xFF, 0xD6, 0x05, 0x09, 0x04])
             self.uid = toHexString(self.response, format=0)
             print (self.uid)
         except SystemError:
             print ("No Card Found")
 
     def write_card_data(self):
-        pass
+        # Direct Transmit
+        WRITE = [0xFF, 0x00, 0xFF, 0x04, 0x0A, 0x0A, 0x03, 0x03]
+        
+    
+        self.response, sw1 = SCardTransmit(self.hcard, self.dwActiveProtocol, COMMAND)
+
+        if self.hresult != SCARD_S_SUCCESS:
+            raise error, 'Failed to transmit: ' + SCardGetErrorMessage(self.hresult)
+        else:
+            print("Successful transmission")
+            print(self.response)
+            print(sw1)
+            
 
 
 if __name__ == '__main__':
     reader = NFC_Reader()
     reader.read_uid()
-
+    reader.read_card_data()
+    reader.write_card_data()
     reader.read_card_data()
 
 
